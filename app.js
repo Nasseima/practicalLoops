b81d5a7e2407681e3156157efee037549c1f5a0d
Nasseima <lindsaynasseima@gmail.com>
2024-05-17 09:46:02 -0400

    Completed Part 3 of practical loops

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Part 1: Fizz Buzz
 // Loop through all numbers from 1 to 100
 num = 101
 for(let i = 1; i < num; i++){
@@ -24,7 +25,7 @@ function fizzBuzz(num){
 }
 //console.log(fizzBuzz(100))
 
-// Prime number
+// Part 2: Prime number
 function checkPrime(start){
     let i = 2;
 
@@ -46,3 +47,8 @@ function findNextPrimeNumber(startingNum){
     return starting;
 }
 console.log(findNextPrimeNumber(3))
+
+// Part 3: Feeling loopy
+const str = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
+const strCopy = str.split("\n");
+console.log(strCopy);
